0b5bab6b865ca37f460f461845af331d5e6a1fd5
antonshvec <shvecanton1987@gmail.com>
2022-12-13 20:46:54 +0100

    Create script.js

---
+++ Продвинутый курс/homework2/JavaScript/script.js
@@ -0,0 +1,59 @@
+// Домашка№2 13.12.22
+// Швец Антон
+// Вечерняя группа
+
+// Задача№1 вариант первый
+
+// function pow(number1) {
+//   return Math.pow(number1,2);
+// }
+
+// console.log(pow(4));
+
+// Задача№1 вариант второй
+
+// const pow = (number1) => Math.pow(number1,2);
+
+// console.log(pow(2));
+
+// Задача№2
+
+// const numbers = (num1,num2,num3,num4,num5) => Math.max(num1,num2,num3,num4,num5);
+
+// console.log(numbers(1,55,3,4,5));
+
+// задача№3
+
+// const absolut = (a) => Math.abs(a);
+// console.log(absolut(-1));
+
+//Задача№4
+
+// const data = (name,surname,age) => {
+//     if (age >=18) {
+//         return console.log(`Здравствуйте, ${name}  ${surname}`)
+//     } else 
+//         return console.log(`Привет, ${name}  ${surname}`);
+// }
+
+// data('Алексей','Аксенов',17);
+
+//Задача№5
+
+// const length_string = (string) => string.length;
+// console.log(length_string('Привет, Антон'));
+
+//Задача№6
+
+// const high_reg = (string) => string.toUpperCase();
+// console.log(high_reg('привет антон'));
+
+//Задача№7
+
+// const maximal_string = (string,string2) => {
+//     if (string.length > string2.length) {
+//         return console.log(string);
+//     } else return console.log(string2);
+// }
+
+// maximal_string('fs3424234fds','dasdadasd');
